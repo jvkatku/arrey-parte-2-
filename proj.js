2a2481e9d90d8c62715153f598440dbc14b8b000
joao <joao_V_rubik@estudante.sesisenai.org.br>
2023-03-15 10:45:06 -0300

    add coisas

--- proj.js
+++ proj.js
@@ -1,12 +1,26 @@
 let valor_cidade = [] // vaiavel global do tipo array
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
 function excluir(){
     valor_cidade.pop()
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 
+}
+function excluir_inicio(){
+    valor_cidade.shift
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
